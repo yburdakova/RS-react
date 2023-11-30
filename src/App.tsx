9b02344f2ea41d7c183608fe8e8102a861af43e7
Yana Burdakova <bu.asti@gmail.com>
2023-11-30 11:59:34 -0500

    feat: create Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,14 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
