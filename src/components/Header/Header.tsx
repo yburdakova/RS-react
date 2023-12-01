c4519f248a31f6e1c6be603d4ff119bd6d4c1d91
Yana Burdakova <bu.asti@gmail.com>
2023-12-01 11:25:05 -0500

    feat: add Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Header.css'
 
 export const Header = () => {
   return (
@@ -7,13 +8,13 @@ export const Header = () => {
       <nav>
         <ul>
           <li>
+            <Link to="/controlled-form">Controlled form</Link>
+          </li>
+          <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/uncontrolled-form">Uncontrolled form</Link>
-          </li>
-          <li>
-            <Link to="/controlled-form">Controlled form</Link>
+          <Link to="/uncontrolled-form">Uncontrolled form</Link>
           </li>
         </ul>
       </nav>
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useAppSelector } from '../hooks/redux';
+import { Link } from 'react-router-dom';
 
 export const NoPage = () => {
+
+  const {error} = useAppSelector(state => state.userReducer);
+
   return (
-    <div>NoPage</div>
+    <div className="nopage">
+      <div className='nopage-container'>
+        <div >
+          <p>This page does not exist!</p>
+          {error}
+        </div>
+        <button>
+          <Link to="/">Сome Back Home</Link>
+        </button>
+      </div>
+    </div>
   )
 }
