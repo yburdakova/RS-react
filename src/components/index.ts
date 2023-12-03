3c032a391804b5c45e22bf52206e1d8ac8b47f9e
Yana Burdakova <bu.asti@gmail.com>
2023-12-03 16:46:17 -0500

    fix: fix 404 error message

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,10 @@
 import { Footer } from "./Footer/Footer";
 import { UserCardItem } from "./UserCardItem/UserCardItem";
-import PasswordStrength from "./PasswordStrength";
-
 
 
 export {
   Footer,
   UserCardItem,
-  PasswordStrength
+
 
 }
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { useAppSelector } from '../hooks/redux';
 import { Link } from 'react-router-dom';
 
 export const NoPage = () => {
 
-  const {error} = useAppSelector(state => state.userReducer);
+
 
   return (
     <div className="nopage">
       <div className='nopage-container'>
         <div >
           <p>This page does not exist!</p>
-          {error}
         </div>
         <button>
           <Link to="/">Come Back Home</Link>
